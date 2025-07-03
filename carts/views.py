7c8e63ef8c8a55674180d476d92b131519487489
Shobika <shobipri57@gmail.com>
2025-07-03 17:59:26 +0530

    paginator in progress

--- carts/views.py
+++ carts/views.py
@@ -37,6 +37,43 @@ def add_cart(request, product_id):
 
     return redirect('cart')
 
+def remove_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(cart_id=_cart_id(request))
+    cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity -= 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product=product,
+            quantity=1,
+            cart=cart
+        )
+        cart_item.save()
+
+    if cart_item.quantity <= 0:
+        cart_item.delete()
+
+    return redirect('cart')
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = Product.objects.get(id=product_id)
+    
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.delete()
+    except CartItem.DoesNotExist:
+        pass
+
+    return redirect('cart')
+
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
@@ -44,6 +81,10 @@ def cart(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.product_price * cart_item.quantity)
             quantity += cart_item.quantity
+
+        tax = (2 * total) / 100  # Assuming a tax rate of 2%
+        grand_total = total + tax
+
     except Cart.DoesNotExist:
         pass
 
@@ -51,6 +92,9 @@ def cart(request, total=0, quantity=0, cart_items=None):
         'total': total,
         'quantity': quantity,
         'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total,
+        'cart_count': cart_items.count() if cart_items else 0,
     }
 
     return render(request, 'store/cart.html', context)
--- store/views.py
+++ store/views.py
@@ -1,7 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
+from carts.models import CartItem
+from carts.views import _cart_id
 from category.models import Category
 from .models import Product
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 
@@ -11,24 +15,33 @@ def store(request, category_slug=None):
     if category_slug is not None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
 
 def product_detail(request, category_slug, product_slug):
     try:
-        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug) # category__slug is the syntax to filter by related model's field
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug) # category__slug is the syntax to filter by related Foreign key model's field
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+        
     except Exception as e:
         raise e
     
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html',context)
